f836fcaf5c6d0f15db5e84367ff6a4acc9baa77f
Tamara <kleiman.tamara@gmail.com>
2015-10-08 13:17:40 -0400

    succesfully removes weak mutation rows

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from numpy import *
-import sys, getopt
+import sys, getopt, os
 
+WEAK_MUTATION_PERCENT = 0.01
 def strip_first_col(fname, delimiter=None):
     with open(fname, 'r') as fin:
         for line in fin:
@@ -28,4 +29,18 @@ def fetch_arg(argv):
 #take out the labels that came with the dataset
 inputfile = fetch_arg(sys.argv[1:])
 data = loadtxt(strip_first_col(inputfile), skiprows=1);
+
+#removing the mutations that cumulatively account for less than 1%
+sortedTotalsForEachMutation = sort(sum(data, 1))
+
+originalSortedIndices = argsort(sum(data,1))
+totalMutations = sum(data)
+totalMutationsToRemove = sum(cumsum(sortedTotalsForEachMutation)/ totalMutations < \
+                                 WEAK_MUTATION_PERCENT)
+originalIndicesToRemove = originalSortedIndices[0: totalMutationsToRemove]
+
+#remove rows
+genomes = data
+genomes = delete(genomes, originalIndicesToRemove, 0)
+
 
